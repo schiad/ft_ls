466a04febb6f37789526c30b9e8149873561c7c6
schiad <schiad@student.42.fr>
2016-12-25 15:00:06 +0100

    new structure bug

--- rendu/ft_ls.c
+++ rendu/ft_ls.c
@@ -10,6 +10,7 @@ int	main(int argc, char **argv)
 		ft_list(".");
 	else
 		ft_list(argv[1]);
+	return (0);
 }
 
 t_file	*ft_lstfadd(t_file *input, struct dirent *file, char *path)
@@ -19,9 +20,23 @@ t_file	*ft_lstfadd(t_file *input, struct dirent *file, char *path)
 	{
 		if (!(tmp = (t_file *)malloc(sizeof(t_file))))
 			return ((void *)NULL);
-		tmp->name = file;
+		tmp->d_ino = file->d_ino;
+		tmp->d_off = file->d_off;
+		tmp->d_reclen = file->d_reclen;
+		tmp->d_type = file->d_type;
+		tmp->d_name = ft_strdup(file->d_name);
+//		tmp->d_name = file->d_name;
+		ft_putstr(tmp->d_name);
 		tmp->path = path;
 		tmp->next = NULL;
+		tmp->dir = 0;
+		ft_insp_file(tmp);
+		if ((tmp->st_mode & S_IFMT) == S_IFDIR)
+		{
+			if (0 == ft_strequ(tmp->d_name, "..") &&
+			0 == ft_strequ(tmp->d_name, "."))
+				tmp->dir = 1;
+		}
 		return tmp;
 	}
 	else
@@ -40,12 +55,10 @@ int	ft_list(char *path)
 {
 	t_file			*files;
 	DIR				*dir;
-	t_file			*doss;
 	t_file			*tmp;
 	struct	dirent	*tmp2;
 
 	files	= NULL;
-	doss	= NULL;
 
 	ft_putstr("\n\n");
 	ft_putstr(path);
@@ -65,16 +78,11 @@ int	ft_list(char *path)
 	tmp = files;
 	while (tmp)
 	{
-		ft_insp_file(tmp);
-		switch (tmp->prop->st_mode & S_IFMT)
+		switch (tmp->st_mode & S_IFMT)
 		{
 			case S_IFBLK:	ft_putstr("block device");		break;
 			case S_IFCHR:	ft_putstr("character device");	break;
-			case S_IFDIR:	
-				ft_putstr("directory");
-				if (!ft_strequ(tmp->name->d_name, "..") && !ft_strequ(tmp->name->d_name, "."))
-					doss = ft_lstfadd(doss, tmp->name, path);
-				break;
+			case S_IFDIR:	ft_putstr("directory");			break;
 			case S_IFIFO:	ft_putstr("FIFO/pipe");			break;
 			case S_IFLNK:	ft_putstr("symlink");			break;
 			case S_IFREG:	ft_putstr("regular file");		break;
@@ -82,32 +90,48 @@ int	ft_list(char *path)
 			default:		ft_putstr("unknown?");
 		}
 		ft_putstr("\t");
-		ft_putstr(tmp->name->d_name);
+		ft_putstr(tmp->d_name);
+		ft_putstr("\t");
+		ft_putstr(tmp->path);
 		ft_putstr("\n");
 		tmp = tmp->next;
 	}
 	closedir(dir);
-	while (doss)
+	tmp = files;
+	while (tmp)
 	{
-		if (ft_strlen(doss->name->d_name))
-			ft_list(ft_strjoin(path, ft_strjoin("/", doss->name->d_name)));
-		doss = doss->next;
+		if (tmp->dir)
+			ft_list(ft_strjoin(path, ft_strjoin("/", tmp->d_name)));
+		tmp = tmp->next;
 	}
 }
 
 void	ft_insp_file(t_file *file)
 {
-	char *tmp;
-	char *pathfile;
+	char		*tmp;
+	char		*pathfile;
+	struct stat	*prop;
 
 	tmp = ft_strjoin(file->path, "/");
-	pathfile = ft_strjoin(tmp, file->name->d_name);
-	file->prop = (struct stat *)malloc(sizeof(struct stat) + 1);
-	if (lstat(pathfile, file->prop) < 0)
+	pathfile = ft_strjoin(tmp, file->d_name);
+	if (lstat(pathfile, prop) < 0)
 	{
 		ft_putstr("\e[41m");
 		ft_putstr("error = ");
 		ft_putstr(strerror(errno));
 		ft_putstr("\e[0m");
 	}
+	file->st_dev = prop->st_dev;
+	file->st_ino = prop->st_ino;
+	file->st_mode = prop->st_mode;
+	file->st_nlink = prop->st_nlink;
+	file->st_uid = prop->st_uid;
+	file->st_gid = prop->st_gid;
+	file->st_rdev = prop->st_rdev;
+	file->st_size = prop->st_size;
+	file->st_blksize = prop->st_blksize;
+	file->st_blocks = prop->st_blocks;
+	file->st_atim = prop->st_atim;
+	file->st_mtim = prop->st_mtim;
+	file->st_ctim = prop->st_ctim;
 }
--- rendu/ft_ls.h
+++ rendu/ft_ls.h
@@ -19,10 +19,27 @@
 
 typedef	struct	s_file
 {
-	struct	stat	*prop;
-	struct	dirent	*name;
-	struct	s_file	*next;
 	char			*path;
+	char			*d_name;
+	int				dir;
+	ino_t			d_ino;
+	off_t			d_off;
+	unsigned short	d_reclen;
+	unsigned char	d_type;
+	dev_t			st_dev;
+	ino_t			st_ino;
+	mode_t			st_mode;
+	nlink_t			st_nlink;
+	uid_t			st_uid;
+	gid_t			st_gid;
+	dev_t			st_rdev;
+	off_t			st_size;
+	blksize_t		st_blksize;
+	blkcnt_t		st_blocks;
+	struct timespec st_atim;
+	struct timespec st_mtim;
+	struct timespec st_ctim;
+	struct	s_file	*next;
 }				t_file;
 
 t_file  *ft_lstfadd(t_file *input, struct dirent *file, char *path);
