01c9a74ab75c31ce32d93f7b5a8e23d762882da9
schiad <schiad@student.42.fr>
2017-01-01 20:01:10 +0100

    new prints functions

--- rendu/ft_ls.c
+++ rendu/ft_ls.c
@@ -4,168 +4,221 @@
 
 int	main(int argc, char **argv)
 {
-	if (argc != 2)
-		ft_list(".");
-	else
-		ft_list(argv[1]);
-	while (1)
-		;
+		t_flags	*flags;
+
+		flags = (t_flags *)malloc(sizeof(t_flags));
+		flags->l = 1;
+		if (argc != 2)
+				ft_list(".", flags);
+		else
+				ft_list(argv[1], flags);
 }
 
 char	*ft_path_join(const char *str1, const char *str2)
 {
-	char	*tmp1;
-	char	*result;
-	int	i;
-	int	j;
+		char	*tmp1;
+		char	*result;
+		int	i;
+		int	j;
 
-	result = ft_strnew(ft_strlen(str1) + ft_strlen(str2) + 1);
-	i = 0;
-	j = 0;
-	while (str1[i])
-	{
-		if (!(str1[i] == '/' && (str1[i + 1] == '/' || str1[i + 1] == '\0')))
+		result = ft_strnew(ft_strlen(str1) + ft_strlen(str2) + 1);
+		i = 0;
+		j = 0;
+		while (str1[i])
 		{
-			result[j++] = str1[i];
+				if (!(str1[i] == '/' && (str1[i + 1] == '/' || str1[i + 1] == '\0')))
+				{
+						result[j++] = str1[i];
+				}
+				i++;
 		}
-		i++;
-	}
-	result[j] = '/';
-	j++;
-	i = 0;
-	while (str2[i])
-	{
-		result[j++] = str2[i];
-		i++;
-	}
-	result[j] = '\0';
+		result[j] = '/';
+		j++;
+		i = 0;
+		while (str2[i])
+		{
+				result[j++] = str2[i];
+				i++;
+		}
+		result[j] = '\0';
 
-	return result;
+		return result;
 }
 
 t_file	*ft_lstfadd(t_file *input, struct dirent *file, char *path)
 {
-	t_file	*tmp;
-	if (!input)
-	{
-		if (!(tmp = (t_file *)malloc(sizeof(t_file))))
-			return ((void *)NULL);
-		tmp->name = file;
-		tmp->path = path;
-		tmp->doss = 0;
-		tmp->next = NULL;
-		return tmp;
-	}
-	else
-	{
-		tmp = input;
-		while (tmp->next)
+		t_file	*tmp;
+		if (!input)
 		{
-			tmp = tmp->next;
+				if (!(tmp = (t_file *)malloc(sizeof(t_file))))
+						return ((void *)NULL);
+				tmp->name = file;
+				tmp->path = path;
+				tmp->doss = 0;
+				tmp->next = NULL;
+				return tmp;
 		}
-		tmp->next = ft_lstfadd(NULL, file, path);
-	}
-	return input;
+		else
+		{
+				tmp = input;
+				while (tmp->next)
+				{
+						tmp = tmp->next;
+				}
+				tmp->next = ft_lstfadd(NULL, file, path);
+		}
+		return input;
 }
 
 void	ft_lstffree(t_file *input)
 {
-	t_file	*tmp;
-	t_file	*tmp2;
+		t_file	*tmp;
+		t_file	*tmp2;
 
-	tmp = input;
-	tmp2 = NULL;
+		tmp = input;
+		tmp2 = NULL;
 
-	while (tmp)
-	{
-		tmp2 = tmp;
-		tmp = tmp->next;
-		ft_putstr_fd("Free prop\n", 2);
-		free(tmp2->prop);
-		free(tmp2);
-	}
+		while (tmp)
+		{
+				tmp2 = tmp;
+				tmp = tmp->next;
+				free(tmp2->prop);
+				free(tmp2);
+		}
 }
 
-int	ft_list(char *path)
+void	ft_elemright(mode_t	mode)
 {
-	t_file		*files;
-	DIR		*dir;
-	t_file		*tmp;
-	struct	dirent	*tmp2;
-	char		*tmppath;
+		int		i;
+		int		div[3];
+		char	str[4];
 
-	files	= NULL;
-
-	ft_putstr(path);
-	ft_putstr(":\n");
-
-	dir = opendir(path);
-	if (dir == NULL)
-	{
-		ft_putstr_fd(strerror(errno), 2);
-		ft_putstr_fd("\n\r", 2);
-		return errno;
-	}
-	while ((tmp2 = readdir(dir)) != NULL)
-	{
-		files = ft_lstfadd(files, tmp2, path);
-	}
-	tmp = files;
-	while (tmp)
-	{
-		ft_insp_file(tmp);
-		switch (tmp->prop->st_mode & S_IFMT)
+		i = 0;
+		div[0] = 1;
+		div[1] = 10;
+		div[2] = 100;
+		str[3] = '\0';
+		while (i < 3)
 		{
-			case S_IFBLK:	ft_putstr("block device");		break;
-			case S_IFCHR:	ft_putstr("character device");	break;
-			case S_IFDIR:	
-					ft_putstr("directory");
-					if ((ft_strequ(tmp->name->d_name, "..") == 0) && (ft_strequ(tmp->name->d_name, ".") == 0))
-						tmp->doss = 1;
-					break;
-			case S_IFIFO:	ft_putstr("FIFO/pipe");			break;
-			case S_IFLNK:	ft_putstr("symlink");			break;
-			case S_IFREG:	ft_putstr("regular file");		break;
-			case S_IFSOCK:	ft_putstr("socket");			break;
-			default:		ft_putstr("unknown?");
+				str[0] = (mode & (S_IRUSR / div[i])) ? 'r' : '-';
+
+				str[1] = (mode & (S_IWUSR / div[i])) ? 'w' : '-';
+				if (mode & S_IXUSR >> i)
+				{
+						str[2] = (mode & (S_ISUID / div[i])) ? 's' : 'x';
+				}
+				else
+				{
+						str[2] = (mode & (S_ISUID / div[i])) ? 'S' : '-';
+				}
+				if (i == 2)
+				{
+						if (str[2] == 's' || str[2] == 'S')
+								str[2]++;
+				}
+				ft_putstr(str);
+				i++;
 		}
-		ft_putstr("\t");
-		ft_putstr(tmp->name->d_name);
-		ft_putstr("\n");
-		tmp = tmp->next;
-	}
-	tmp = files;
-	while (tmp)
-	{
-		if (tmp->doss)
+		ft_putchar('\t');
+}
+
+char	*ft_elemtype(t_file	*line)
+{
+		if (S_ISREG(line->prop->st_mode))
+				return ("-");
+		else if (S_ISDIR(line->prop->st_mode))
+				return ("d");
+		else if (S_ISCHR(line->prop->st_mode))
+				return ("c");
+		else if (S_ISBLK(line->prop->st_mode))
+				return ("b");
+		else if (S_ISFIFO(line->prop->st_mode))
+				return ("p");
+		else if (S_ISLNK(line->prop->st_mode))
+				return ("l");
+		else if (S_ISSOCK(line->prop->st_mode))
+				return ("s");
+}
+
+void	ft_elemname(t_file *line)
+{
+		ft_putstr(line->name->d_name);
+		ft_putchar('\n');
+}
+
+void	ft_printline(t_file	*line, t_flags	*flags)
+{
+		if (flags->l)
 		{
-			if (ft_strlen(tmp->name->d_name))
-			{
-				ft_putstr("\n");
-				ft_list(tmppath = ft_path_join(path, tmp->name->d_name));
-				ft_strdel(&tmppath);
-			}
+				ft_putstr(ft_elemtype(line));
+				ft_elemright(line->prop->st_mode);
 		}
-		tmp = tmp->next;
-	}
-	closedir(dir);
-	ft_lstffree(files);
+		ft_elemname(line);
+}
+
+int	ft_list(char *path, t_flags *flags)
+{
+		t_file		*files;
+		DIR		*dir;
+		t_file		*tmp;
+		struct	dirent	*tmp2;
+		char		*tmppath;
+
+		files	= NULL;
+
+		ft_putstr(path);
+		ft_putstr(":\n\n");
+
+		dir = opendir(path);
+		if (dir == NULL)
+		{
+				ft_putstr_fd(strerror(errno), 2);
+				ft_putstr_fd("\n\r", 2);
+				return errno;
+		}
+		while ((tmp2 = readdir(dir)) != NULL)
+		{
+				files = ft_lstfadd(files, tmp2, path);
+		}
+		tmp = files;
+		while (tmp)
+		{
+				ft_insp_file(tmp);
+				ft_printline(tmp ,flags);
+				tmp = tmp->next;
+		}
+		tmp = files;
+		while (tmp)
+		{
+				if (tmp->doss)
+				{
+						if (ft_strlen(tmp->name->d_name))
+						{
+								ft_putstr("\n");
+								ft_list(tmppath = ft_path_join(path, tmp->name->d_name), flags);
+								ft_strdel(&tmppath);
+						}
+				}
+				tmp = tmp->next;
+		}
+		closedir(dir);
+		ft_lstffree(files);
 }
 
 void	ft_insp_file(t_file *file)
 {
-	char *tmp;
-	char *pathfile;
+		char *tmp;
+		char *pathfile;
 
-	tmp = ft_strjoin(file->path, "/");
-	pathfile = ft_strjoin(tmp, file->name->d_name);
-	file->prop = (struct stat *)malloc(sizeof(struct stat));
-	if (lstat(pathfile, file->prop) < 0)
-	{
-		ft_putstr_fd("\e[41m", 2);
-		ft_putstr_fd("error = ", 2);
-		ft_putstr_fd(strerror(errno), 2);
-		ft_putstr_fd("\e[0m", 2);
-	}
-	ft_strdel(&pathfile);
+		tmp = ft_strjoin(file->path, "/");
+		pathfile = ft_strjoin(tmp, file->name->d_name);
+		file->prop = (struct stat *)malloc(sizeof(struct stat));
+		if (lstat(pathfile, file->prop) < 0)
+		{
+				ft_putstr_fd("\e[41m", 2);
+				ft_putstr_fd("error = ", 2);
+				ft_putstr_fd(strerror(errno), 2);
+				ft_putstr_fd("\e[0m", 2);
+		}
+		ft_strdel(&pathfile);
 }
--- rendu/ft_ls.h
+++ rendu/ft_ls.h
@@ -17,6 +17,15 @@
 # include <errno.h>
 # include "libft.h"
 
+typedef	struct	s_flags
+{
+	int				a;
+	int				l;
+	int				r;
+	int				R;
+	int				t;
+}				t_flags;
+
 typedef	struct	s_file
 {
 	struct	stat	*prop;
@@ -28,8 +37,12 @@ typedef	struct	s_file
 
 t_file  *ft_lstfadd(t_file *input, struct dirent *file, char *path);
 void    ft_lstffree(t_file *input);
-int	ft_list(char *path);
+int		ft_list(char *path, t_flags *flags);
+void    ft_elemright(mode_t	mode);
+char    *ft_elemtype(t_file *line);
+void    ft_printline(t_file *line, t_flags *flags);
 void	ft_insp_file(t_file *file);
+void	ft_elemname(t_file *line);
 char	*ft_path_join(const char *str1, const char *str2);
 
 #endif
