cc5452c7f51942a3f9f9f79da699ac6afd7330ca
Salim Chiad <salim@kim.chiad.fr>
2017-01-15 10:30:32 +0100

    Total added

--- rendu/ft_ls.c
+++ rendu/ft_ls.c
@@ -4,334 +4,375 @@
 
 int	main(int argc, char **argv)
 {
-	t_flags	*flags;
+		t_flags	*flags;
 
-	flags = (t_flags *)malloc(sizeof(t_flags));
-	flags->l = 1;
-	flags->R = 1;
-	if (argc != 2)
-		ft_list(".", flags);
-	else
-		ft_list(argv[1], flags);
-	return 0;
+		flags = (t_flags *)malloc(sizeof(t_flags));
+		flags->l = 1;
+		flags->R = 1;
+		flags->exec = argv[0];
+		if (argc != 2)
+				ft_list(".", flags);
+		else
+				ft_list(argv[1], flags);
+		ft_memdel((void **)&flags);
+		return 0;
 }
 
 char	*ft_path_join(const char *str1, const char *str2)
 {
-	char	*tmp1;
-	char	*result;
-	int	i;
-	int	j;
+		char	*tmp1;
+		char	*result;
+		int	i;
+		int	j;
 
-	result = ft_strnew(ft_strlen(str1) + ft_strlen(str2) + 1);
-	i = 0;
-	j = 0;
-	while (str1[i])
-	{
-		if (!(str1[i] == '/' && (str1[i + 1] == '/' || str1[i + 1] == '\0')))
+		result = ft_strnew(ft_strlen(str1) + ft_strlen(str2) + 1);
+		i = 0;
+		j = 0;
+		while (str1[i])
 		{
-			result[j++] = str1[i];
+				if (!(str1[i] == '/' && (str1[i + 1] == '/' || str1[i + 1] == '\0')))
+				{
+						result[j++] = str1[i];
+				}
+				i++;
 		}
-		i++;
-	}
-	result[j] = '/';
-	j++;
-	i = 0;
-	while (str2[i])
-	{
-		result[j++] = str2[i];
-		i++;
-	}
-	result[j] = '\0';
+		result[j] = '/';
+		j++;
+		i = 0;
+		while (str2[i])
+		{
+				result[j++] = str2[i];
+				i++;
+		}
+		result[j] = '\0';
 
-	return result;
+		return result;
 }
 
 t_file	*ft_lstfadd(t_file *input, struct dirent *file, char *path)
 {
-	t_file	*tmp;
-	if (!input)
-	{
-		if (!(tmp = (t_file *)malloc(sizeof(t_file))))
-			return ((void *)NULL);
-		if (!(tmp->name = (struct dirent *)malloc(sizeof(struct dirent))))
-			return ((void *)NULL);
-		//		tmp->name = file;
-		ft_memcpy((void *)tmp->name, (void *)file, sizeof(struct dirent));
-		tmp->path = path;
-		tmp->doss = 0;
-		tmp->next = NULL;
-		return tmp;
-	}
-	else
-	{
-		tmp = input;
-		while (tmp->next)
+		t_file	*tmp;
+		if (!input)
 		{
-			tmp = tmp->next;
+				if (!(tmp = (t_file *)malloc(sizeof(t_file))))
+						return ((void *)NULL);
+				if (!(tmp->name = (struct dirent *)malloc(sizeof(struct dirent))))
+						return ((void *)NULL);
+				ft_memcpy((void *)tmp->name, (void *)file, sizeof(struct dirent));
+				tmp->path = path;
+				tmp->doss = 0;
+				tmp->next = NULL;
+				return tmp;
 		}
-		tmp->next = ft_lstfadd(NULL, file, path);
-	}
-	return input;
+		else
+		{
+				tmp = input;
+				while (tmp->next)
+				{
+						tmp = tmp->next;
+				}
+				tmp->next = ft_lstfadd(NULL, file, path);
+		}
+		return input;
 }
 
 void	ft_lstffree(t_file *input)
 {
-	t_file	*tmp;
-	t_file	*tmp2;
+		t_file	*tmp;
+		t_file	*tmp2;
 
-	tmp = input;
-	tmp2 = NULL;
+		tmp = input;
+		tmp2 = NULL;
 
-	while (tmp)
-	{
-		tmp2 = tmp;
-		tmp = tmp->next;
-		free(tmp2->name);
-		free(tmp2->prop);
+		while (tmp)
+		{
+				tmp2 = tmp;
+				tmp = tmp->next;
+				free(tmp2->name);
+				free(tmp2->prop);
 
-		free(tmp2);
-	}
+				free(tmp2);
+		}
 }
 
 void	ft_elemright(mode_t	mode)
 {
-	ft_rightuser(mode);
-	ft_rightgroup(mode);
-	ft_rightother(mode);
-	ft_putchar('\t');
+		ft_rightuser(mode);
+		ft_rightgroup(mode);
+		ft_rightother(mode);
+		ft_putchar('\t');
 }
 
 void	ft_rightuser(mode_t	mode)
 {
-	char	str[4];
+		char	str[4];
 
-	str[3] = '\0';
-	str[0] = (mode & S_IRUSR) ? 'r' : '-';
-	str[1] = (mode & S_IWUSR) ? 'w' : '-';
-	if (mode & S_IXUSR)
-	{
-		str[2] = (mode & S_ISUID) ? 's' : 'x';
-	}
-	else
-	{
-		str[2] = (mode & S_ISUID) ? 'S' : '-';
-	}
-	ft_putstr(str);
+		str[3] = '\0';
+		str[0] = (mode & S_IRUSR) ? 'r' : '-';
+		str[1] = (mode & S_IWUSR) ? 'w' : '-';
+		if (mode & S_IXUSR)
+		{
+				str[2] = (mode & S_ISUID) ? 's' : 'x';
+		}
+		else
+		{
+				str[2] = (mode & S_ISUID) ? 'S' : '-';
+		}
+		ft_putstr(str);
 }
 
 void	ft_rightgroup(mode_t	mode)
 {
-	char	str[4];
+		char	str[4];
 
-	str[3] = '\0';
-	str[0] = (mode & S_IRGRP) ? 'r' : '-';
-	str[1] = (mode & S_IWGRP) ? 'w' : '-';
-	if (mode & S_IXGRP)
-	{
-		str[2] = (mode & S_ISGID) ? 's' : 'x';
-	}
-	else
-	{
-		str[2] = (mode & S_ISGID) ? 'S' : '-';
-	}
-	ft_putstr(str);
+		str[3] = '\0';
+		str[0] = (mode & S_IRGRP) ? 'r' : '-';
+		str[1] = (mode & S_IWGRP) ? 'w' : '-';
+		if (mode & S_IXGRP)
+		{
+				str[2] = (mode & S_ISGID) ? 's' : 'x';
+		}
+		else
+		{
+				str[2] = (mode & S_ISGID) ? 'S' : '-';
+		}
+		ft_putstr(str);
 }
 
 void	ft_rightother(mode_t	mode)
 {
-	char	str[4];
+		char	str[4];
 
-	str[3] = '\0';
-	str[0] = (mode & S_IROTH) ? 'r' : '-';
-	str[1] = (mode & S_IWOTH) ? 'w' : '-';
-	if (mode & S_IXOTH)
-	{
-		str[2] = (mode & S_ISVTX) ? 't' : 'x';
-	}
-	else
-	{
-		str[2] = (mode & S_ISVTX) ? 'T' : '-';
-	}
-	ft_putstr(str);
+		str[3] = '\0';
+		str[0] = (mode & S_IROTH) ? 'r' : '-';
+		str[1] = (mode & S_IWOTH) ? 'w' : '-';
+		if (mode & S_IXOTH)
+		{
+				str[2] = (mode & S_ISVTX) ? 't' : 'x';
+		}
+		else
+		{
+				str[2] = (mode & S_ISVTX) ? 'T' : '-';
+		}
+		ft_putstr(str);
 }
 
 char	*ft_elemtype(t_file	*line)
 {
-	if (S_ISREG(line->prop->st_mode))
-		return ("-");
-	else if (S_ISDIR(line->prop->st_mode))
-		return ("d");
-	else if (S_ISCHR(line->prop->st_mode))
-		return ("c");
-	else if (S_ISBLK(line->prop->st_mode))
-		return ("b");
-	else if (S_ISFIFO(line->prop->st_mode))
-		return ("p");
-	else if (S_ISLNK(line->prop->st_mode))
-		return ("l");
-	else if (S_ISSOCK(line->prop->st_mode))
-		return ("s");
-	return ("U");
+		if (S_ISREG(line->prop->st_mode))
+				return ("-");
+		else if (S_ISDIR(line->prop->st_mode))
+				return ("d");
+		else if (S_ISCHR(line->prop->st_mode))
+				return ("c");
+		else if (S_ISBLK(line->prop->st_mode))
+				return ("b");
+		else if (S_ISFIFO(line->prop->st_mode))
+				return ("p");
+		else if (S_ISLNK(line->prop->st_mode))
+				return ("l");
+		else if (S_ISSOCK(line->prop->st_mode))
+				return ("s");
+		return ("U");
 }
 
 void	ft_elemname(t_file *line)
 {
-	ft_putstr(line->name->d_name);
-	ft_putchar('\n');
+		ft_putstr(line->name->d_name);
+		ft_putchar('\n');
 }
 
 void	ft_elemowner(t_file *line, t_flags *flags)
 {
-	struct passwd *usr;
-	struct group *grp;
+		struct passwd *usr;
+		struct group *grp;
 
-	usr = getpwuid(line->prop->st_uid);
-	grp = getgrgid(line->prop->st_gid);
-	if (usr)
-		ft_putstr(usr->pw_name);
-	else
-		ft_putllong((long long)line->prop->st_uid);
-	ft_putchar(' ');
-	if (grp)
-		ft_putstr(grp->gr_name);
-	else
-		ft_putllong((long long)line->prop->st_gid);
-	ft_putchar(' ');
+		usr = getpwuid(line->prop->st_uid);
+		grp = getgrgid(line->prop->st_gid);
+		if (usr)
+				ft_putstr(usr->pw_name);
+		else
+				ft_putllong((long long)line->prop->st_uid);
+		ft_putchar(' ');
+		if (grp)
+				ft_putstr(grp->gr_name);
+		else
+				ft_putllong((long long)line->prop->st_gid);
+		ft_putchar(' ');
 }
 
 void	ft_elemsize(t_file	*line)
 {
-	if (S_ISCHR(line->prop->st_mode) || S_ISBLK(line->prop->st_mode))
-	{
-		ft_putnbr(major(line->prop->st_rdev));
-		ft_putstr(", ");
-		ft_putnbr(minor(line->prop->st_rdev));
-		ft_putchar(' ');
-	}
-	else
-	{
-		ft_putllong(line->prop->st_size);
-		ft_putchar('\t');
-	}
+		if (S_ISCHR(line->prop->st_mode) || S_ISBLK(line->prop->st_mode))
+		{
+				ft_putnbr(major(line->prop->st_rdev));
+				ft_putstr(", ");
+				ft_putnbr(minor(line->prop->st_rdev));
+				ft_putchar(' ');
+		}
+		else
+		{
+				ft_putllong(line->prop->st_size);
+				ft_putchar('\t');
+		}
 }
 
 void	ft_printline(t_file	*line, t_flags	*flags)
 {
-	if (flags->l)
-	{
-		ft_putstr(ft_elemtype(line));
-		ft_elemright(line->prop->st_mode);
-		ft_putnbr((int)line->prop->st_nlink);
-		ft_putchar(' ');
-		ft_elemowner(line, flags);
-		ft_elemsize(line);
-		ft_display_date(line->prop->st_mtime);
-	}
-	ft_elemname(line);
+		if (line->error)
+		{
+				ft_putstr_fd(flags->exec, 2);
+				ft_putstr_fd(": cannot access ", 2);
+				ft_putstr_fd(line->path, 2);
+				ft_putstr_fd("/", 2);
+				ft_putstr_fd(line->name->d_name, 2);
+				ft_putstr_fd(": ", 2);
+				ft_putstr_fd(strerror(errno), 2);
+				ft_putstr_fd("\e[0m\n", 2);
+		}
+		else
+		{
+				if (flags->l)
+				{
+						ft_putstr(ft_elemtype(line));
+						ft_elemright(line->prop->st_mode);
+						ft_putnbr((int)line->prop->st_nlink);
+						ft_putchar(' ');
+						ft_elemowner(line, flags);
+						ft_elemsize(line);
+						ft_display_date(line->prop->st_mtime);
+				}
+				ft_elemname(line);
+		}
 }
 
 void	ft_display_date(time_t date)
 {
-	char	*str1;
-	char	*str2;
-	time_t	actualtime;
+		char	*str1;
+		char	*str2;
+		time_t	actualtime;
 
-	actualtime = time(0);
-	str1 = ctime(&date);
-	if ((actualtime - 15778463) > date || actualtime < date)
-	{
-		str2 = ft_strnew(6);
-		str2 = ft_strsub(str1, 20, 4);
-		str1 = ft_strsub(str1, 4, 6);
-		str1 = ft_strjoin(str1, "  ");
-		str1 = ft_strjoin(str1, str2);
-		free(str2);
-	}
-	else
-		str1 = ft_strsub(str1, 4, 12);
-	str1[12] = '\0';
-	ft_putstr(str1);
-	ft_putchar(' ');
-	free(str1);
+		actualtime = time(0);
+		str1 = ctime(&date);
+		if ((actualtime - 15778463) > date || actualtime < date)
+		{
+				str2 = ft_strnew(6);
+				str2 = ft_strsub(str1, 20, 4);
+				str1 = ft_strsub(str1, 4, 6);
+				str1 = ft_strjoin(str1, "  ");
+				str1 = ft_strjoin(str1, str2);
+				free(str2);
+		}
+		else
+				str1 = ft_strsub(str1, 4, 12);
+		str1[12] = '\0';
+		ft_putstr(str1);
+		ft_putchar(' ');
+		free(str1);
+}
+
+void	ft_printtotal(t_file *files, t_flags *flags)
+{
+		t_file		*tmp;
+		long long	total;
+
+		tmp = files;
+		total = 0;
+		if (flags->l)
+		{
+				ft_putstr("total ");
+				while (tmp)
+				{
+					total = total + tmp->prop->st_blocks;
+					tmp = tmp->next;
+				}
+				ft_putllong(total);
+				ft_putstr("\n");
+		}
 }
 
 int	ft_list(char *path, t_flags *flags)
 {
-	t_file		*files;
-	DIR		*dir;
-	t_file		*tmp;
-	struct	dirent	*tmp2;
-	char		*tmppath;
+		t_file			*files;
+		DIR				*dir;
+		t_file			*tmp;
+		struct	dirent	*tmp2;
+		char			*tmppath;
 
-	files	= NULL;
+		files	= NULL;
 
-	if (flags->R)
-	{
-		ft_putstr(path);
-		ft_putstr(":\n\n");
-	}
-
-	dir = opendir(path);
-	if (dir == NULL)
-	{
-		ft_putstr_fd(strerror(errno), 2);
-		ft_putstr_fd("\n\r", 2);
-		return errno;
-	}
-	while ((tmp2 = readdir(dir)) != NULL)
-	{
-		files = ft_lstfadd(files, tmp2, path);
-	}
-	tmp = files;
-	while (tmp)
-	{
-		if(!ft_insp_file(tmp))
-			ft_printline(tmp ,flags);
-		tmp = tmp->next;
-	}
-	tmp = files;
-	while (tmp && flags->R)
-	{
-		if (tmp->doss)
+		dir = opendir(path);
+		if (dir == NULL)
 		{
-			if (ft_strlen(tmp->name->d_name))
-			{
-				ft_putstr("\n");
-				ft_list(tmppath = ft_path_join(path, tmp->name->d_name), flags);
-				ft_strdel(&tmppath);
-			}
+				ft_putstr_fd(flags->exec, 2);
+				ft_putstr_fd(": cannot open directory '", 2);
+				ft_putstr_fd(path, 2);
+				ft_putstr_fd("': ", 2);
+				ft_putstr_fd(strerror(errno), 2);
+				ft_putstr_fd("\n", 2);
+				return errno;
 		}
-		tmp = tmp->next;
-	}
-	closedir(dir);
-	ft_lstffree(files);
-	return 0;
+		while ((tmp2 = readdir(dir)) != NULL)
+				files = ft_lstfadd(files, tmp2, path);
+		if (flags->R)
+		{
+				ft_putstr(path);
+				ft_putstr(":\n");
+		}
+		tmp = files;
+		while (tmp)
+		{
+				ft_insp_file(tmp, flags);
+				tmp = tmp->next;
+		}
+		ft_printtotal(files, flags);
+		tmp = files;
+		while (tmp)
+		{
+			if (!tmp->error)
+				ft_printline(tmp ,flags);
+			tmp = tmp->next;
+		}
+
+		tmp = files;
+		while (tmp && flags->R)
+		{
+				if (tmp->doss)
+				{
+						if (ft_strlen(tmp->name->d_name))
+						{
+								ft_putstr("\n");
+								ft_list(tmppath = ft_path_join(path, tmp->name->d_name), flags);
+								ft_strdel(&tmppath);
+						}
+				}
+				tmp = tmp->next;
+		}
+		closedir(dir);
+		ft_lstffree(files);
+		return 0;
 }
 
-int		ft_insp_file(t_file *file)
+int		ft_insp_file(t_file *file, t_flags *flags)
 {
-	char *tmp;
-	char *pathfile;
+		char *tmp;
+		char *pathfile;
 
-	tmp = ft_strjoin(file->path, "/");
-	pathfile = ft_strjoin(tmp, file->name->d_name);
-	file->prop = (struct stat *)malloc(sizeof(struct stat));
-	if (lstat(pathfile, file->prop) < 0)
-	{
-		ft_putstr_fd("\e[41m", 2);
-		ft_putstr_fd("ft_ls: ", 2);
-		ft_putstr_fd(file->name->d_name, 2);
-		ft_putstr_fd(": ", 2);
-		ft_putstr_fd(strerror(errno), 2);
-		ft_putstr_fd("\e[0m\n", 2);
-		return 1;
-	}
-	file->doss = 0;
-	if ((ft_strequ(file->name->d_name, "..") == 0) &&
-			(ft_strequ(file->name->d_name, ".") == 0))
-		if (S_ISDIR(file->prop->st_mode))
-			file->doss = 1;
-	ft_strdel(&pathfile);
-	return 0;
+		tmp = ft_strjoin(file->path, "/");
+		pathfile = ft_strjoin(tmp, file->name->d_name);
+		file->prop = (struct stat *)malloc(sizeof(struct stat));
+		if (lstat(pathfile, file->prop) < 0)
+		{
+				file->error = 1;
+				return 1;
+		}
+		file->doss = 0;
+		if ((ft_strequ(file->name->d_name, "..") == 0) &&
+						(ft_strequ(file->name->d_name, ".") == 0))
+				if (S_ISDIR(file->prop->st_mode))
+						file->doss = 1;
+		ft_strdel(&tmp);
+		ft_strdel(&pathfile);
+		file->error = 0;
+		return 0;
 }
--- rendu/ft_ls.h
+++ rendu/ft_ls.h
@@ -19,21 +19,23 @@
 
 typedef	struct		s_flags
 {
-	int		a;
-	int		l;
-	int		r;
-	int		R;
-	int		t;
-}			t_flags;
+	int				a;
+	int				l;
+	int				r;
+	int				R;
+	int				t;
+	char			*exec;
+}					t_flags;
 
 typedef	struct		s_file
 {
 	struct	stat	*prop;
 	struct	dirent	*name;
 	struct	s_file	*next;
-	int		doss;
-	char		*path;
-}			t_file;
+	int				doss;
+	char			*path;
+	int				error;
+}					t_file;
 
 t_file  *ft_lstfadd(t_file *input, struct dirent *file, char *path);
 void    ft_lstffree(t_file *input);
@@ -46,7 +48,8 @@ char    *ft_elemtype(t_file *line);
 void    ft_elemowner(t_file *line, t_flags *flags);
 void    ft_printline(t_file *line, t_flags *flags);
 void	ft_display_date(time_t date);
-int		ft_insp_file(t_file *file);
+void    ft_printtotal(t_file *files, t_flags *flags);
+int		ft_insp_file(t_file *file, t_flags *flags);
 void	ft_elemname(t_file *line);
 char	*ft_path_join(const char *str1, const char *str2);
 
